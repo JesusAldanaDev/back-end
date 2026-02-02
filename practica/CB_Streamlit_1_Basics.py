f20f90c2620eae00f605806df781930e9511878a
Jesus Aldana <jesus.aldana0623@protonmail.com>
2026-02-02 04:33:10 -0500

    Agregando mas cosas a la practica de streamlit

--- practica/CB_Streamlit_1_Basics.py
+++ practica/CB_Streamlit_1_Basics.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
 
 def get_df():
@@ -18,34 +20,71 @@ def get_df():
 st.title("Streamlit Basics")
 st.markdown("a brief project on Streamlit widgets :sunglasses:")
 st.divider()
-df = get_df()
 
-st.data_editor(df)
+generate_df = st.checkbox("Generate DataFrame")
+if generate_df:
+    df = get_df()
+
+
+st.dataframe(df)
+download_button = st.download_button(
+    label="Download DataFrame",
+    data=df.to_csv(),
+    file_name="DataFrame.csv",
+)
 
 df_max = df.max().max()
 df_min = df.min().min()
 df_mean = df.mean().mean()
 
-st.metric(
-    "Max Value",
-    df_max,
-    delta=None,
-    delta_color="normal",
-    help="The max value in the dataframe",
+metrics_selections = st.multiselect(
+    label="select metrics to show",
+    options=["Max Value", "Min Value", " average"],
+    placeholder="choose an option",
 )
 
-st.metric(
-    "min Value",
-    df_min,
-    delta=None,
-    delta_color="normal",
-    help="The min value in the dataframe",
-)
+if "Max Value" in metrics_selections:
+    st.metric(
+        "Max Value",
+        df_max,
+        delta=f"{df_max}",
+        delta_color="normal",
+        help="The max value in the dataframe",
+    )
+if "Min Value" in metrics_selections:
+    st.metric(
+        "min Value",
+        df_min,
+        delta=f"{df_min}",
+        delta_color="normal",
+        help="The min value in the dataframe",
+    )
+if " average" in metrics_selections:
+    st.metric(
+        "mean Value",
+        df_mean,
+        delta=None,
+        delta_color="normal",
+        help="The mean value in the dataframe",
+    )
 
-st.metric(
-    "mean Value",
-    df_mean,
-    delta=None,
-    delta_color="normal",
-    help="The mean value in the dataframe",
+plot_type = st.radio(
+    label="Select plot library",
+    options=["Matplotlib", "Plotly 2D", "Plotly 3D"],
 )
+if "matplotlib" in plot_type:
+    fig = plt.figure()
+    contour = plt.contour(df)
+    plt.colorbar(contour)
+    st.pyplot(fig)
+
+if "Plotly 2D" in plot_type:
+    fig2 = go.Figure(data=go.Contour(z=df))
+    st.plotly_chart(fig2)
+
+if "Plotly" in plot_type:
+    fig2 = go.Figure(data=go.Surface(z=df))
+    st.plotly_chart(fig2)
+
+
+# min 48
